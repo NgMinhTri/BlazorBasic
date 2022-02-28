35d10f993ceaba3cdc3469d665e60bfd963ea28f
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-02-28 12:31:24 +0700

    Create form and validate form

--- Blazorbasic.Models/TaskCreateRequest.cs
+++ Blazorbasic.Models/TaskCreateRequest.cs
@@ -1,12 +1,18 @@
 ﻿using Blazorbasic.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blazorbasic.Models
 {
     public class TaskCreateRequest
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [MaxLength(20, ErrorMessage = "You cannot fill task name over than 20 characters")]
+        [Required(ErrorMessage = "Please enter your task name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select your task priority")]
         public Priority? Priority { get; set; }
     }
 }
