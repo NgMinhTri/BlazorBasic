61cc8e1d9d34c6018ed71c067dee67014caf9978
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-03-01 20:39:01 +0700

    bắt sự kiện từ component cha tới component con

--- BlazorAssemblyBasic/Pages/Tasks.razor.cs
+++ BlazorAssemblyBasic/Pages/Tasks.razor.cs
@@ -1,8 +1,6 @@
 ﻿using BlazorAssemblyBasic.Services;
 using Blazorbasic.Models;
-using Blazored.Toast.Services;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,23 +9,19 @@ namespace BlazorAssemblyBasic.Pages
     public partial class Tasks
     {
         [Inject] private ITaskApiClient TaskApiClient { set; get; }
-        [Inject] private IUserApiClient UserApiClient { set; get; }
-        [Inject] private IToastService ToastService { set; get; }
 
         private List<TaskDto> taskList;
-        private List<AssignerDto> Assigners;
         private TaskListSearch TaskListSearch = new TaskListSearch();
 
         protected override async Task OnInitializedAsync()
         {
             taskList = await TaskApiClient.GetTaskList(TaskListSearch);
-            Assigners = await UserApiClient.GetAssigners();
         }
 
 
-        private async Task SearchForm(EditContext context)
+        public async Task SearchTask(TaskListSearch taskListSearch)
         {
-            ToastService.ShowInfo("Search completed", "Info");
+            TaskListSearch = taskListSearch;
             taskList = await TaskApiClient.GetTaskList(TaskListSearch);
         }
 
